4bffbd619c4908bb6fdf5e8effd617cc48dea9f6
gmatta-2021223 <gmatta-2021223@kinal.edu.gt>
2024-10-20 18:58:31 -0600

    Finalización del proyecto de números aleatorios.

--- programa.cpp
+++ programa.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main() {
 
     srand(time(NULL)); // Semilla para números aleatorios
     
@@ -13,15 +13,53 @@ int main(int argc, char **argv) {
     int limiteSuperior = 9;
 
     int array[5]; // Array de 5 enteros
+    // {0,0,0,0,0}
 
     for (int i = 0; i < 5; i++) {
         numeroRandom = limiteInferior + rand() % (limiteSuperior + 1 - limiteInferior);
         array[i] = numeroRandom;
-
     }
 
+    // {3,4,1,6,9}
+
     for (int i = 0; i < 5; i++) {
-        cout << array[i] << "\n";
+        cout << array[i] << "  ";
+    }
+
+    // Inciso a). Calcualr si el numero tiene digitos repetidos o todos los digitos son diferentes (no se repiten)
+
+    bool digitosRepetidos = false;
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            /* cout << "\nComparando " << array[i] << " con " << array[j]; */
+            if (array[i] == array[j]) {
+                digitosRepetidos = true;
+                break;
+            }
+        }
+    }
+
+    if (digitosRepetidos) {
+        cout << "\nEl numero tiene digitos repetidos";
+    } else {
+        cout << "\nEl numero NO tiene digitos repetidos";
+    }
+
+    // Inciso b) y c). Calcular que números de 0 a 9 se repiten en el número generado
+
+    int contador[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    // {0,0,0,0,0,0,0,0,0,0}
+    // {1,1,0,1,0,1,2,0,0,0}
+
+    for (int i = 0; i < 5; i++) {
+        contador[array[i]]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (contador[i] > 0) {
+            cout << "\nEl numero " << i << " se repite " << contador[i] << " veces";
+        }
     }
 
     return 0;
